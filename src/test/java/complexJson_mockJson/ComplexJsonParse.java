049cb19ebdf9870f3ad0a8ab7366bc53f3893793
malathi <malathi@Deepak>
2025-03-15 22:44:17 +0530

    Second commit

--- src/test/java/complexJson_mockJson/ComplexJsonParse.java
+++ src/test/java/complexJson_mockJson/ComplexJsonParse.java
@@ -10,7 +10,8 @@ public class ComplexJsonParse {
 	public static void main(String[] args) {
 		// Check File Mock_json_Used & Payload
 
-		JsonPath js = new JsonPath(Payload.CoursePrice());// successfully mocked the response when your Pojo_API is not ready
+		JsonPath js = new JsonPath(Payload.CoursePrice());// successfully mocked the response when your Pojo_API is not
+															// ready
 
 		// 1. Print No of courses returned by Pojo_API
 		int count = js.getInt("courses.size()");// can be applied only on arrays
@@ -36,35 +37,34 @@ public class ComplexJsonParse {
 			String courseTitles = js.get("courses[" + i + "].title");// +i+ is used to dynamically insert the value of
 																		// the variable i into the string.
 			System.out.println(courseTitles);
-			
+
 			// System.out.println(js.get("courses["+i+"].price")); //this statemet wud throw
 			// error so we dnt know return type so to avoid we will just add .toString() so
 			// that the return type would be in string format
-			
+
 			System.out.println(js.get("courses[" + i + "].price").toString());
-			
-		//	System.out.println(courseTitles + ": " + js.get("courses[" + i + "].price").toString());
+
+			// System.out.println(courseTitles + ": " + js.get("courses[" + i +
+			// "].price").toString());
 		}
 
 		// 5. Print no of copies sold by RPA Course
-		
+
 		System.out.println("");
 		for (int i = 0; i < count; i++) {
 			String courseTitles = js.get("courses[" + i + "].title");
-			if(courseTitles.equalsIgnoreCase("RPA"))
-			{
-				//return copies sold
+			if (courseTitles.equalsIgnoreCase("RPA")) {
+				// return copies sold
 				int copies = js.get("courses[" + i + "].copies");
 				System.out.println("Print no of copies sold by RPA Course: " + copies);
 				break;
 			}
-			}
+		}
 
 		// 6. Verify if Sum of all Course prices matches with Purchase Amount
-		
+
 		// Check in SumOfAllCourses
-		
-		
+
 	}
 
 }
